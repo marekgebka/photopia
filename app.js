65bb60b0dcf2dd83c6eebacabb09850f535a7a49
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-06-26 20:10:30 +0530

    installed body-parser

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express'),
-    app = express();
-
+    app = express(),
+    bodyParser = require('body-parser');
 
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
   res.render('landing');
