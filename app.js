7ffbfad9c729d4d565652848d2a6bea65fe9831c
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-06-26 20:33:20 +0530

    simple form to upload new images

--- app.js
+++ app.js
@@ -6,22 +6,32 @@ var express = require('express'),
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+var gallery = [
+  {name: 'Central', image: 'https://source.unsplash.com/random/500x500'},
+  {name: 'Maximize', image: 'https://source.unsplash.com/random/500x501'},
+  {name: 'Business-focused', image: 'https://source.unsplash.com/random/500x502'}
+]
+
+
 app.get('/', function(req, res){
   res.render('landing');
 });
 
 app.get('/gallery', function(req, res){
-  var gallery = [
-    {name: 'Central', image: 'https://source.unsplash.com/random/500x500'},
-    {name: 'Maximize', image: 'https://source.unsplash.com/random/500x501'},
-    {name: 'Business-focused', image: 'https://source.unsplash.com/random/500x502'}
-  ]
-
   res.render('gallery', {gallery : gallery});
 });
 
 app.post('/gallery', function(req, res){
-  res.send('Gallery POST Route');
+  var name = req.body.name,
+      image = req.body.image,
+      newImage = {
+        name: name,
+        image: image
+      };
+  
+  gallery.push(newImage);
+  res.redirect('/gallery');
 });
 
 app.get('/gallery/new', function(req, res){
