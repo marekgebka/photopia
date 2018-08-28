8b4e37c448578c1ac0684769d3efaf3aed71f80e
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-08-28 07:19:56 +0530

    tested mongoose with local mongodb

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
-
+mongoose.connect('mongodb://localhost:27017/photopia', {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+// Schema Setup
+var photoSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Photo = mongoose.model("Photo", photoSchema);
+
+Photo.create({
+  name: 'Maximize',
+  image: 'https://source.unsplash.com/random/500x501'
+}, function(err, photo){
+  if(err){
+    console.log(err);
+  } else {
+    console.log("Newly created photo");
+    console.log(photo);
+  }
+});
 
 
 var gallery = [
