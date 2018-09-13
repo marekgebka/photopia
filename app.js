768fc28e57953e0ff0e33ec47981ecc3cdc1ea1f
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-09-13 17:54:03 +0530

    added public directory

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var express = require('express'),
 mongoose.connect('mongodb://localhost:27017/photopia', {useNewUrlParser: true});
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 seedDB();
 
 app.get('/', function(req, res){
