6cde0f2231b8fc5c9931ae5fe9fab80ce7ebf4ab
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-09-13 23:29:04 +0530

    refactored routes

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ var express = require('express'),
     Photo = require('./models/photo'),
     Comment = require('./models/comment'),
     User = require('./models/user'),
-    seedDB = require('./seeds')
+    seedDB = require('./seeds');
 
+// Requiring express router routes
+var commentRoutes = require('./routes/comments'),
+    galleryRoutes = require('./routes/gallery'),
+    indexRoutes = require('./routes/index')
 
 
 mongoose.connect('mongodb://localhost:27017/photopia', {useNewUrlParser: true});
@@ -34,132 +38,10 @@ app.use(function(req, res, next){
   next();
 });
 
-
-app.get('/', function(req, res){
-  res.render('landing');
-});
-
-// Index
-app.get('/gallery', function(req, res){
-  Photo.find({}, function(err, allPhotos){
-    if(err){
-      console.log(err);
-    } else {
-      res.render('gallery/index', {gallery : allPhotos});
-    }
-  });
-});
-
-// Create
-app.post('/gallery', function(req, res){
-  var name = req.body.name,
-      image = req.body.image,
-      desc = req.body.description
-      newImage = {
-        name: name,
-        image: image,
-        description: desc
-      };
-  Photo.create(newImage, function(err, newImage){
-    if(err){
-      console.log(err)
-    } else {
-      res.redirect('/gallery');
-    }
-  });
-});
-
-// New
-app.get('/gallery/new', function(req, res){
-  res.render('gallery/new');
-});
-
-// Show
-app.get('/gallery/:id', function(req, res){
-  Photo.findById(req.params.id).populate('comments').exec(function(err, foundPhoto){
-    if(err){
-      console.log(err);
-    } else {
-      res.render('gallery/show', {photo: foundPhoto});
-    }
-  });
-});
-
-// ===========================================================
-// Comments Routes
-// ===========================================================
-
-app.get('/gallery/:id/comments/new',isLoggedIn, function(req, res){
-  Photo.findById(req.params.id, function(err, gallery){
-    if(err){
-      console.log(err);
-    } else {
-      res.render('comments/new', {gallery: gallery});
-    }
-  });
-});
-
-app.post('/gallery/:id/comments',isLoggedIn, function(req, res){
-  Photo.findById(req.params.id, function(err, gallery){
-    if(err){
-      console.log(err);
-      res.redirect('/gallery');
-    } else {
-      Comment.create(req.body.comment, function(err, comment){
-        if(err){
-          console.log(err);
-        } else {
-          gallery.comments.push(comment);
-          gallery.save();
-          res.redirect('/gallery/' + gallery._id);
-        }
-      });
-    }
-  });
-});
-
-//==================
-// Auth Routes
-// =================
-
-app.get('/register', function(req, res){
-  res.render('register');
-});
-
-app.post('/register', function(req, res){
-  var newUser = new User({username: req.body.username});
-  User.register(newUser, req.body.password, function(err, user){
-    if(err){
-      console.log(err);
-      return res.render('register');
-    }
-    passport.authenticate('local')(req, res, function(){
-      res.redirect('/gallery');
-    });
-  });
-});
-
-app.get('/login', function(req, res){
-  res.render('login');
-});
-
-app.post('/login', passport.authenticate('local', {
-  successRedirect: '/gallery',
-  failureRedirect: '/login'
-}) ,function(req, res){
-});
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/gallery');
-});
-
-function isLoggedIn(req, res, next){
-  if(req.isAuthenticated()){
-    return next();
-  }
-  res.redirect('/login');
-}
+// Express router
+app.use('/', indexRoutes);
+app.use('/gallery/:id/comments',commentRoutes);
+app.use('/gallery', galleryRoutes);
 
 app.listen(3001, function(){
   console.log('The Photopia server has started!');
