4df8ed7a026ca67e5fda14b3d159e8017ee70476
C C Darshan Thimmaiah <darshancc23@gmail.com>
2018-09-13 22:14:48 +0530

    registeration route

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 seedDB();
 
+// Passport Configuration
+app.use(require('express-session')({
+  secret: '*&Scnkjdfy7dsgJGHSCjkjsyd7sd6yu3jhdkszhc7hbn*',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 app.get('/', function(req, res){
   res.render('landing');
 });
@@ -97,6 +110,27 @@ app.post('/gallery/:id/comments', function(req, res){
         }
       });
     }
+  });
+});
+
+//==================
+// Auth Routes
+// =================
+
+app.get('/register', function(req, res){
+  res.render('register');
+});
+
+app.post('/register', function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate('local')(req, res, function(){
+      res.redirect('/gallery');
+    });
   });
 });
 
